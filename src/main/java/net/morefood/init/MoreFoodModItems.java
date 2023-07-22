078c604889e62262b61c24aa3f1fcdc91faaf212
BigShotEmzy <Emilia@DESKTOP-Q08QPSN>
2023-07-22 15:27:58 +0100

    Added more cheese stuff, prtty cool tbh

--- src/main/java/net/morefood/init/MoreFoodModItems.java
+++ src/main/java/net/morefood/init/MoreFoodModItems.java
@@ -4,6 +4,8 @@
  */
 package net.morefood.init;
 
+import net.morefood.item.GrilledCheeseItem;
+import net.morefood.item.CheeseSandwichItem;
 import net.morefood.item.CheeseItem;
 import net.morefood.MoreFoodMod;
 
@@ -12,9 +14,13 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.Registry;
 
 public class MoreFoodModItems {
+	public static Item GRILLED_CHEESE;
 	public static Item CHEESE;
+	public static Item CHEESE_SANDWICH;
 
 	public static void load() {
+		GRILLED_CHEESE = Registry.register(Registry.ITEM, new ResourceLocation(MoreFoodMod.MODID, "grilled_cheese"), new GrilledCheeseItem());
 		CHEESE = Registry.register(Registry.ITEM, new ResourceLocation(MoreFoodMod.MODID, "cheese"), new CheeseItem());
+		CHEESE_SANDWICH = Registry.register(Registry.ITEM, new ResourceLocation(MoreFoodMod.MODID, "cheese_sandwich"), new CheeseSandwichItem());
 	}
 }
---
+++ src/main/java/net/morefood/item/CheeseSandwichItem.java
@@ -0,0 +1,21 @@
+
+package net.morefood.item;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.food.FoodProperties;
+
+public class CheeseSandwichItem extends Item {
+	public CheeseSandwichItem() {
+		super(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(5).saturationMod(2.6f)
+
+				.build()));
+	}
+
+	@Override
+	public int getUseDuration(ItemStack itemstack) {
+		return 32;
+	}
+}
